2092782f783cc75e51a4e92abc4d7ac88b38fd15
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-05-17 15:59:29 +0200

    Add new descriptions to functions

--- files/srcs/utils/ft_convert.cpp
+++ files/srcs/utils/ft_convert.cpp
@@ -12,6 +12,12 @@
 
 #include "../../includes/ft_irc.h"
 
+/**
+	* @brief	Convert a number to a string
+	* @param	int value : The number to convert
+	* @return	std::string : The string representation of the number
+	* @throw	std::invalid_argument If the input value is invalid
+ */
 std::string ft_nbtos(int const &value)
 {
 	std::ostringstream oss;
@@ -22,6 +28,12 @@ std::string ft_nbtos(int const &value)
 	return (oss.str());
 }
 
+/**
+	* @brief	Convert a string to a number
+	* @param	std::string str : The string to convert
+	* @return	int : The number representation of the string
+	* @throw	std::invalid_argument If the input string is invalid
+ */
 int ft_stoi(std::string const &str)
 {
 	int					number;
