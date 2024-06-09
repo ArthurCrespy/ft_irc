63a9fd408e65f0d7f90305353ac75362cc200861
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-06-09 13:51:03 +0200

    New replies + cleanup

--- files/includes/utils/replies.h
+++ files/includes/utils/replies.h
@@ -13,54 +13,53 @@
 #ifndef REPLIES_H
 # define REPLIES_H
 
-/* Error Responses */
-# define ERR_NONICKNAMEGIVEN(nickname)							"431 " + nickname + " :No nickname given"
-# define RPL_NOTOPIC(nickname, channel)							"331 " + nickname + " " + channel + " :No topic is set"
-# define RPL_TOPIC(nickname, channel, topic)					"332 " + nickname + " " + channel + " :" + topic
-# define ERR_NICKNAMEINUSE(nickname)							"433 " + nickname + " " + nickname	+ " :Nickname is already in use"
-# define ERR_USERNOTINCHANNEL(nickname, channel)				"441 " + nickname + " " + channel + " :They aren't on that channel"
-# define ERR_NOLOGIN(nickname)									"444 " + nickname + " :User not logged in"
-# define ERR_NOTREGISTERED(nickname)							"451 " + nickname + " :You have not registered"
-# define ERR_ALREADYREGISTERED(nickname)						"462 " + nickname + " :You may not register"
-# define ERR_PASSWDMISMATCH(nickname)							"464 " + nickname + " :Password incorrect"
+/* 001-099 | Client-Server Exchange */
+# define RPL_WELCOME(nickname)									"001 " + nickname +						" :Welcome to the 42IRC network"
+# define RPL_YOURHOST(nickname)									"002 " + nickname +						" :Your host is 42IRC, running version 1.0"
 
-# define ERR_UNKNOWNCOMMAND(nickname, command)					"421 " + nickname + " " + command + " :Unknown command"
-# define ERR_NEEDMOREPARAMS(nickname, command)					"461 " + nickname + " " + command + " :Not enough parameters"
+/* 200-399 | Command Replies */
+# define RPL_CHANNELMODEIS(nickname, channel, modes, params)	"324 " + nickname + " " + channel +		" " + modes + " " + params
+# define RPL_NOTOPIC(nickname, channel)							"331 " + nickname + " " + channel +		" :No topic is set"
+# define RPL_TOPIC(nickname, channel, topic)					"332 " + nickname + " " + channel +		" :" + topic
+# define RPL_NAMREPLY(nickname, channel, users)					"353 " + nickname + " = " + channel +	" :" + users
+# define RPL_ENDOFNAMES(nickname, channel)						"366 " + nickname + " " + channel +		" :End of /NAMES list."
 
-# define ERR_NOSUCHCHANNEL(nickname, channel)					"403 " + nickname + " " + channel + " :No such channel"
-# define ERR_CANNOTSENDTOCHAN(nickname, channel)				"404 " + nickname + " " + channel + " :Cannot send to channel"
-# define ERR_TOOMANYCHANNELS(nickname, channel)					"405 " + nickname + " " + channel + " :You have joined too many channels"
-# define ERR_NOTONCHANNEL(nickname, channel)					"442 " + nickname + " " + channel + " :You're not on that channel"
-# define ERR_CHANNELISFULL(nickname, channel)					"471 " + nickname + " " + channel + " :Cannot join channel (+l)"
-# define ERR_BADCHANNELKEY(nickname, channel)					"475 " + nickname + " " + channel + " :Cannot join channel (+k)"
-# define ERR_CHANOPRIVSNEEDED(nickname, channel)				"482 " + nickname + " " + channel + " :You're not channel operator"
+/* 400-599 | Error Replies */
+# define ERR_NOSUCHNICK(nickname, nc)							"401 " + nickname + " " + nc +			" :No such nick/channel"
+# define ERR_NOSUCHCHANNEL(nickname, channel)					"403 " + nickname + " " + channel +		" :No such channel"
+# define ERR_CANNOTSENDTOCHAN(nickname, channel)				"404 " + nickname + " " + channel +		" :Cannot send to channel"
+# define ERR_TOOMANYCHANNELS(nickname, channel)					"405 " + nickname + " " + channel +		" :You have joined too many channels"
+# define ERR_NOTEXTTOSEND(nickname)								"412 " + nickname +						" :No text to send"
+# define ERR_UNKNOWNCOMMAND(nickname, command)					"421 " + nickname + " " + command +		" :Unknown command"
+# define ERR_NICKNAMEINUSE(nickname)							"433 " + nickname + " " + nickname	+	" :Nickname is already in use"
+# define ERR_NONICKNAMEGIVEN(nickname)							"431 " + nickname +						" :No nickname given"
+# define ERR_USERNOTINCHANNEL(nickname, channel)				"441 " + nickname + " " + channel +		" :They aren't on that channel"
+# define ERR_NOTONCHANNEL(nickname, channel)					"442 " + nickname + " " + channel +		" :You're not on that channel"
+# define ERR_NOLOGIN(nickname)									"444 " + nickname +						" :User not logged in"
+# define ERR_NOTREGISTERED(nickname)							"451 " + nickname +						" :You have not registered"
+# define ERR_NEEDMOREPARAMS(nickname, command)					"461 " + nickname + " " + command +		" :Not enough parameters"
+# define ERR_ALREADYREGISTERED(nickname)						"462 " + nickname +						" :You may not register"
+# define ERR_PASSWDMISMATCH(nickname)							"464 " + nickname +						" :Password incorrect"
+# define ERR_CHANNELISFULL(nickname, channel)					"471 " + nickname + " " + channel +		" :Cannot join channel (+l)"
+# define ERR_BADCHANNELKEY(nickname, channel)					"475 " + nickname + " " + channel +		" :Cannot join channel (+k)"
+# define ERR_CHANOPRIVSNEEDED(nickname, channel)				"482 " + nickname + " " + channel +		" :You're not channel operator"
 
-# define ERR_NOSUCHNICK(nickname, nc)							"401 " + nickname + " " + nc + " :No such nick/channel"
-# define ERR_USERNOTINCHANNEL(nickname, channel)				"441 " + nickname + " " + channel + " :They aren't on that channel"
+/* Custom Replies */
+# define RPL_JOIN(channel)										" JOIN :" + channel
+# define RPL_PART(channel)										" PART :" + channel
+# define RPL_PRIVMSG(target, message)							" PRIVMSG " + target + " :" + message
+# define RPL_NOTICE(target, message)							" NOTICE " + target + " :" + message
+# define RPL_QUIT(message)										" QUIT :Quit: " + message
+# define RPL_KICK(channel, target, reason)						" KICK " + channel + " " + target + " :" + reason
+# define RPL_MODE(channel, modes, args)							" MODE " + channel + " " + modes + " " + args
 
-# define ERR_NOTEXTTOSEND(nickname)								"412 " + nickname + " :No text to send"
-
-
-/* Numeric Responses */
-# define RPL_CHANNELMODEIS(nickname, channel, modes, params)	"324 " + nickname + channel + " " + modes + " " + params
-# define RPL_NAMREPLY(nickname, channel, users) /*???*/			"353 " + nickname + " = " + channel + " :" + users
-# define RPL_ENDOFNAMES(nickname, channel)						"366 " + nickname + " " + channel + " :End of /NAMES list."
-
-/* Command Responses */
-# define RPL_JOIN(nickname, channel)							":" + nickname + " JOIN :" + channel
-# define RPL_PART(nickname, channel)							":" + nickname + " PART :" + channel
-# define RPL_PING(nickname, token)								":" + nickname + " PONG :" + token
-# define RPL_PRIVMSG(nickname, target, message)					":" + nickname + " PRIVMSG " + target + " :" + message
-# define RPL_NOTICE(nickname, target, message)					":" + nickname + " NOTICE " + target + " :" + message
-# define RPL_QUIT(nickname, message)							":" + nickname + " QUIT :Quit: " + message
-# define RPL_KICK(nickname, channel, target, reason)			":" + nickname + " KICK " + channel + " " + target + " :" + reason
-# define RPL_MODE(nickname, channel, modes, args)				":" + nickname + " MODE " + channel + " " + modes + " " + args
-
-/* Custom Responses */
-# define RPL_WELCOME(nickname)									"001 " + nickname + " :Welcome to the 42IRC network"
+/* LogBot Replies */
 # define RPL_LBRGST(command)									"Cannot do " + command + "! Not authenticated...\nUse LOGBOT, our bot! /msg LOGBOT <password> <nickname> <username> <realname>"
-# define RPL_LBTREGISTER(nickname)								"Welcome " + nickname + " ! You are now registered"
+# define RPL_LBTREGISTER(nickname)								"Hey " + nickname + " ! You are now registered"
+# define RPL_LBWELCOME(nickname, username, hostname)			"001 " + nickname +	" :Welcome to the 42IRC network " + nickname + "!" + username + "@" + hostname
 
-# define RPL_PONG(nickname) /*test*/							"PONG " + nickname + " :" + nickname
+/* PING/PONG Replies */
+# define RPL_PING(nickname)										"PING " + nickname
+# define RPL_PONG(nickname)										"PONG " + nickname + " :" + nickname
 
 #endif
