37ddb647d08708bf7250a630f46a6693c0b250bd
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-06-11 18:03:03 +0200

    Completely refactoring mode()

--- files/srcs/commands/Server_mode.cpp
+++ files/srcs/commands/Server_mode.cpp
@@ -6,49 +6,97 @@
 /*   By: jdegluai <jdegluai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 14:36:45 by acrespy           #+#    #+#             */
-/*   Updated: 2024/06/11 15:19:42 by jdegluai         ###   ########.fr       */
+/*   Updated: 2024/06/11 17:47:12 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/ft_irc.h"
 
-Channel	*Server::findchannelname(std::string name) {
-	t_channel::iterator	it = this->_channel.find(name);
-
-	if (it == this->_channel.end())
-		return (NULL);
-	return &(it->second);
-}
-
 void Server::mode(int fd, std::string const &msg)
 {
-	std::deque<std::string> args = this->split(msg, " ");
-	if (args.size() < 2)
-		return servSend(fd, -1, ERR_NEEDMOREPARAMS(_client.at(fd)->getNickname(), "MODE"));
-	if (args[1].at(0) != '#' && !args[1].compare(_client.at(fd)->getNickname()))
-		return servSend(fd, -1, RPL_UMODEIS(_client.at(fd)->getNickname(), "+wi"));
+	char action;
+	std::string channel_name;
+	std::string modes;
+	std::string param;
+	std::istringstream iss(msg);
 
-	Channel *channel = this->findchannelname(args[1]);
+	iss >> channel_name >> modes;
 
-	if (!channel)
-		return servSend(fd, -1, ERR_NOSUCHCHANNEL(_client.at(fd)->getNickname(), args[1]));
-	// if (!channel->isInChannel(_client.at(fd), fd))
-	// 	return servSend(fd, -1, ERR_NOTONCHANNEL(_client.at(fd)->getNickname(), args[1]));
-	if (args.size() == 2) {
-		return servSend(fd, -1, RPL_CHANNELMODEIS(_client.at(fd)->getNickname(), args[1], channel->getMode()));
+	if (channel_name.empty() || modes.empty())
+		return (servSend(_srv_sock, fd, ERR_NEEDMOREPARAMS(_client.at(fd)->getNickname(), "MODE")));
+	if (channel_name[0] != '#' && channel_name[0] != '&')
+		return (servSend(_srv_sock, fd, ERR_NOSUCHCHANNEL(_client.at(fd)->getNickname(), channel_name)));
+
+	try
+	{
+		Channel &channel = _channel.at(channel_name.substr(1));
+		action = modes[0];
+		if (action != '+' && action != '-')
+			return (servSend(_srv_sock, fd, ERR_UNKNOWNMODE(_client.at(fd)->getNickname(), modes)));
+
+		modeMulti(fd, iss, channel, modes);
+
+		std::string response = ":" + _client.at(fd)->getNickname() + " MODE " + channel_name + " " + modes + " " + param;
+		channel.broadcast(_client.at(fd)->getNickname(), response);
 	}
-	if (!channel->isAdmins(_client.at(fd)))
-		return servSend(fd, -1, ERR_CHANOPRIVSNEEDED(_client.at(fd)->getNickname(), channel->getName()));
-	
-	std::string	modes = args[2];
-	std::string	availableModes = "itkol+-";
-	bool		updatedModes = false;
+	catch (const std::out_of_range &)
+	{
+		servSend(_srv_sock, fd, ERR_NOSUCHCHANNEL(_client.at(fd)->getNickname(), channel_name));
+	}
+}
 
-	if (availableModes.find(modes[0]) == std::string::npos)
-		return servSend(fd, -1, ERR_UNKNOWNMODE(_client.at(fd)->getNickname(), toString(modes[0])));
-	
-	(void)updatedModes;
-	(void)msg;
-	(void)fd;
-	
+void Server::modeMulti(int fd, std::istringstream &iss, Channel &channel, std::string &modes)
+{
+	std::string param;
+	char action = modes[0];
+
+	for (size_t i = 1; i < modes.length(); ++i)
+	{
+		char mode = modes[i];
+		if (mode == 'i')
+			channel.setInviteOnly(action == '+');
+		else if (mode == 't')
+			channel.setTopicRestriction(action == '+');
+		else if (mode == 'k')
+			modeK(fd, iss, channel, action);
+		else if (mode == 'o')
+			modeO(fd, iss, channel, action);
+		else
+			servSend(_srv_sock, fd, ERR_UNKNOWNMODE(_client.at(fd)->getNickname(), std::string(1, mode)));
+	}
+}
+
+void Server::modeK(int fd, std::istringstream &iss, Channel &channel, char action)
+{
+	std::string param;
+
+	if (action == '+')
+	{
+		if (!(iss >> param))
+			return (servSend(_srv_sock, fd, ERR_NEEDMOREPARAMS(_client.at(fd)->getNickname(), "MODE")));
+		channel.setPassword(param);
+		channel.setPasswordRestriction(true);
+	}
+	else
+		channel.setPasswordRestriction(false);
+}
+
+void Server::modeO(int fd, std::istringstream &iss, Channel &channel, char action)
+{
+	std::string param;
+
+	if (!(iss >> param))
+		return (servSend(_srv_sock, fd, ERR_NEEDMOREPARAMS(_client.at(fd)->getNickname(), "MODE")));
+	try
+	{
+		Client &client = _user.at(param);
+		if (action == '+')
+			channel.addAdmin(&client);
+		else
+			channel.removeAdmin(&client);
+	}
+	catch (const std::out_of_range &)
+	{
+		servSend(_srv_sock, fd, ERR_NOSUCHNICK(_client.at(fd)->getNickname(), param));
+	}
 }
