00f2216adc046587376f7e35eec447858eaed8b3
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-06-13 18:57:10 +0200

    Refactor nick

--- files/srcs/commands/Server_nick.cpp
+++ files/srcs/commands/Server_nick.cpp
@@ -14,22 +14,25 @@
 
 void Server::nick(int fd, std::string const &msg)
 {
+	std::string result;
 	std::string nickname;
 	std::istringstream iss(msg);
+	Client *client = _client.find(fd)->second;
 
 	iss >> nickname;
+	result = client->getUsername().empty() ? client->getHostname() : client->getUsername();
 	if (nickname[0] == ':')
 		nickname.erase(0, 1);
 	if (nickname.empty())
 		servSend(_srv_sock, fd, ERR_NONICKNAMEGIVEN(_client.at(fd)->getNickname()));
-	else if (_user.find(nickname) != _user.end())
-		servSend(_srv_sock, fd, ERR_NICKNAMEINUSE(nickname));
-	else if (!_client.find(fd)->second->getRegistration())
-		_client.find(fd)->second->setNickname(nickname);
+	else if (isClient(nickname))
+		servSend(_srv_sock, fd, ERR_NICKNAMEINUSE(result, nickname));
+	else if (!client->getRegistration())
+		client->setNickname(nickname);
 	else
 	{
-		std::string old_nickname = _client.find(fd)->second->getNickname();
-		_client.find(fd)->second->setNickname(nickname);
+		std::string old_nickname = client->getNickname();
+		client->setNickname(nickname);
 		servSend(_srv_sock, fd, RPL_NICK(old_nickname, nickname));
 	}
 }
