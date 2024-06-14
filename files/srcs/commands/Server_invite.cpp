bc5a216c8f65f0ee5e963dac076a740ecdf1ada1
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-06-14 12:31:04 +0200

    Merge fix

--- files/srcs/commands/Server_invite.cpp
+++ files/srcs/commands/Server_invite.cpp
@@ -14,17 +14,17 @@
 
 void Server::invite(int fd, std::string const &msg)
 {
-	std::string nickname;
+	std::string guestname;
 	std::string channel_name;
 	std::istringstream iss(msg);
 
-	iss >> nickname >> channel_name;
+	iss >> guestname >> channel_name;
 	if (channel_name[0] == '#' || channel_name[0] == '&')
 		channel_name.erase(0, 1);
-	if (channel_name.empty() || nickname.empty())
+	if (channel_name.empty() || guestname.empty())
 		return (servSend(_srv_sock, fd, ERR_NEEDMOREPARAMS(_client.find(fd)->second->getNickname(), "KICK")));
 
-	if (!isClient(nickname))
+	if (!isClient(guestname))
 		servSend(_srv_sock, fd, ERR_NOSUCHNICK(_client.at(fd)->getNickname(), msg));
 	else if (_channel.count(channel_name) != 0)
 	{ 
@@ -34,16 +34,16 @@ void Server::invite(int fd, std::string const &msg)
 			if (!channel.isAdmin(_client.at(fd)))
 				return (servSend(_srv_sock, fd, ERR_CHANOPRIVSNEEDED(_client.find(fd)->second->getNickname(), channel_name)));
 		}
-		servSend(_srv_sock, _user.at(guestname).getFd(), ":" + _client.at(fd)->getNickname() + " INVITE " + guestname + " :" + channel_name);
-		channel.addMember(&getClient(nickname));
-		channel.broadcast(nickname, nickname + " has joined the channel.");
+		servSend(_srv_sock, getClient(guestname).getFd(), ":" + _client.at(fd)->getNickname() + " INVITE " + guestname + " :" + channel_name);
+		channel.addMember(&getClient(guestname));
+		channel.broadcast(guestname, guestname + " has joined the channel.");
 	}
 	else
 	{
-    	servSend(_srv_sock, _user.at(guestname).getFd(), ":" + _client.at(fd)->getNickname() + " INVITE " + guestname + " :" + channel_name);
-    	Channel channel_new(channel_name, &getClient(nickname));
+    	servSend(_srv_sock, getClient(guestname).getFd(), ":" + _client.at(fd)->getNickname() + " INVITE " + guestname + " :" + channel_name);
+    	Channel channel_new(channel_name, &getClient(guestname));
     	_channel.insert(std::make_pair(channel_name, channel_new));
-    	servSend(_srv_sock, _user.at(guestname).getFd(), RPL_JOIN(nickname, channel_name));
+    	servSend(_srv_sock, getClient(guestname).getFd(), RPL_JOIN(guestname, channel_name));
 	}
 	servSend(_srv_sock, fd, RPL_INVITING(_client.find(fd)->second->getNickname(), guestname, channel_name));
 }
