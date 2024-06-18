2c3070b48c1ce378d888a4d457e0bdd35a90b1da
CRESPY Arthur <47624245+ArthurCrespy@users.noreply.github.com>
2024-06-18 10:45:22 +0200

    Typo in invite()

--- files/srcs/commands/Server_invite.cpp
+++ files/srcs/commands/Server_invite.cpp
@@ -14,18 +14,18 @@
 
 void Server::invite(int fd, std::string const &msg)
 {
-	std::string guestname;
+	std::string guest_name;
 	std::string channel_name;
 	std::istringstream iss(msg);
 	Client *client = _client.at(fd);
 
-	iss >> guestname >> channel_name;
+	iss >> guest_name >> channel_name;
 	if (channel_name[0] == '#' || channel_name[0] == '&')
 		channel_name.erase(0, 1);
-	if (channel_name.empty() || guestname.empty())
+	if (channel_name.empty() || guest_name.empty())
 		return (servSend(_srv_sock, fd, ERR_NEEDMOREPARAMS(client->getNickname(), "KICK")));
 
-	if (!isClient(guestname))
+	if (!isClient(guest_name))
 		servSend(_srv_sock, fd, ERR_NOSUCHNICK(client->getNickname(), msg));
 	else if (_channel.count(channel_name) != 0)
 	{
@@ -35,9 +35,9 @@ void Server::invite(int fd, std::string const &msg)
 			if (!channel.isAdmin(client))
 				return (servSend(_srv_sock, fd, ERR_CHANOPRIVSNEEDED(client->getNickname(), channel_name)));
 		}
-		channel.addInvite(&getClient(guestname));
-		servSend(fd, fd, RPL_INVITING(client->getNickname(), channel_name, guestname));
-		servSend(fd, getClient(guestname).getFd(), RPL_INVITING(client->getNickname(), channel_name, guestname));
+		channel.addInvite(&getClient(guest_name));
+		servSend(fd, fd, RPL_INVITING(client->getNickname(), channel_name, guest_name));
+		servSend(fd, getClient(guest_name).getFd(), RPL_INVITING(client->getNickname(), channel_name, guest_name));
 	}
 	else
     	servSend(_srv_sock, fd, ERR_NOTONCHANNEL(client->getNickname(), channel_name));
